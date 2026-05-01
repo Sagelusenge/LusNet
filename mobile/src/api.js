39affd2c9296df6fd4211be429785e18f0374852
Sage KITSA <sagelusenge@gmail.com>
2026-05-01 20:38:10 +0200

    Upgrade mobile app to Expo SDK 54

--- mobile/src/api.js
+++ mobile/src/api.js
@@ -1,6 +1,6 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const API_BASE_URL = process.env.EXPO_PUBLIC_API_BASE_URL || 'http://192.168.0.21:3000/api';
+const API_BASE_URL = process.env.EXPO_PUBLIC_API_BASE_URL || 'https://lwasiva-net.onrender.com/api';
 
 export async function getToken() {
   return AsyncStorage.getItem('lwasiva_token');
